681f193128eaecc58e2a709738a7d831e2af24ff
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2019-03-27 23:04:01 +0900

    add tab

---
+++ src/components/month/tab/MonthTab.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-tabs centered>
+    <v-tab>
+      Result
+    </v-tab>
+    <v-tab>
+      Calendar
+    </v-tab>
+    <v-tab>
+      Details
+    </v-tab>
+  </v-tabs>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class MonthTab extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
