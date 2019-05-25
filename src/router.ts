e613b56a35c10da7860be930d2b9e25e94bd0b6e
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2019-05-25 16:08:47 +0900

    add detail routing

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Input from '@/components/Input.vue';
 
 Vue.use(Router);
 
@@ -25,6 +24,12 @@ export default new Router({
       name: 'calendar',
       component: () =>
         import('@/components/month/calendar/MonthCalendar.vue'),
+    },
+    {
+      path: '/detail',
+      name: 'detail',
+      component: () =>
+        import('@/components/month/detail/MonthDetail.vue'),
     },
   ],
 });
