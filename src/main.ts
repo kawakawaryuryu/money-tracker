a36870eddaca5d18e042e8645d5eddc09cd2e99e
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2019-05-26 17:37:44 +0900

    add vuex

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import App from './App.vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 import router from '@/router';
+import store from '@/store';
 
 Vue.config.productionTip = false;
 Vue.use(Vuetify);
 
 new Vue({
   router,
+  store,
   render: (h) => h(App),
 
 }).$mount('#app');
---
+++ src/store.ts
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { CLOSE_INPUT_DIALOG, OPEN_INPUT_DIALOG } from '@/mutation-types';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    inputDialog: false,
+  },
+  mutations: {
+    [OPEN_INPUT_DIALOG](state) {
+      state.inputDialog = true;
+    },
+    [CLOSE_INPUT_DIALOG](state) {
+      state.inputDialog = false;
+    },
+  },
+  actions: {
+
+  },
+});
