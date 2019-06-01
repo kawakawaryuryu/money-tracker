bbf7bc5881b4e197bf5de5340ac721c380815640
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2019-06-02 00:56:11 +0900

    separate input component

--- src/components/input/Input.vue
+++ src/components/input/Input.vue
@@ -12,47 +12,9 @@
             <span class="headline">Input expense</span>
           </v-card-title>
         </v-layout>
-        <v-layout justify-center>
-          <v-flex xs12 sm8 md6>
-            <v-text-field
-                label="Amount"
-                type="number"
-                prefix="¥"
-            ></v-text-field>
-          </v-flex>
-        </v-layout>
-        <v-layout justify-center>
-          <v-flex xs12 sm8 md6>
-            <v-menu
-                v-model="menu"
-                :close-on-content-click="false"
-                lazy
-                transition="scale-transition"
-                full-width
-            >
-              <template v-slot:activator="{ on }">
-                <v-text-field
-                    v-model="date"
-                    label="Date"
-                    prepend-inner-icon="event"
-                    readonly
-                    v-on="on"
-                ></v-text-field>
-              </template>
-              <v-date-picker
-                  v-model="date"
-                  @input="menu = false"
-              ></v-date-picker>
-            </v-menu>
-          </v-flex>
-        </v-layout>
-        <v-layout justify-center>
-          <v-flex xs12 sm8 md6>
-            <v-text-field
-                label="Content"
-            ></v-text-field>
-          </v-flex>
-        </v-layout>
+        <InputAmount></InputAmount>
+        <InputDate></InputDate>
+        <InputContent></InputContent>
         <v-layout row justify-center>
           <v-flex xs6 sm4 md2>
             <v-btn flat @click="cancel()">Cancel</v-btn>
@@ -69,12 +31,18 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { CLOSE_INPUT_DIALOG } from '@/mutation-types';
+import InputAmount from '@/components/input/amount/InputAmount.vue';
+import InputDate from '@/components/input/date/InputDate.vue';
+import InputContent from '@/components/input/content/InputContent.vue';
 
-@Component
+@Component({
+  components: {
+    InputAmount,
+    InputDate,
+    InputContent,
+  },
+})
 export default class Input extends Vue {
-  private date = new Date().toISOString().substr(0, 10);
-  private menu: boolean = false;
-
   public get dialog(): boolean {
     return this.$store.state.inputDialog;
   }
---
+++ src/components/input/date/InputDate.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-layout justify-center>
+    <v-flex xs12 sm8 md6>
+      <v-menu
+          v-model="menu"
+          :close-on-content-click="false"
+          lazy
+          transition="scale-transition"
+          full-width
+      >
+        <template v-slot:activator="{ on }">
+          <v-text-field
+              v-model="date"
+              label="Date"
+              prepend-inner-icon="event"
+              readonly
+              v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+            v-model="date"
+            @input="menu = false"
+        ></v-date-picker>
+      </v-menu>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class InputDate extends Vue {
+    private date = new Date().toISOString().substr(0, 10);
+    private menu: boolean = false;
+
+  }
+</script>
+
+<style scoped>
+
+</style>
